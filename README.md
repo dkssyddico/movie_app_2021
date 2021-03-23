7cc2522211fe6b4021296950b1e8d76d106650f7
dkssyddico <dkssyddico@naver.com>
2021-03-23 22:44:04 +0900

    chapter4 complete

--- README.md
+++ README.md
@@ -359,7 +359,30 @@ getMovies = async () => {
 };
 ```
 
+<br>
+
 ### async, await 쓰는 이유
 
 - axios.get으로 데이터를 받아오는 게 느릴 수 있음
 - 완전히 데이터를 받아오고 처리하기 위해서
+
+<br>
+
+### Invaild Dom property 'class' 오류
+
+- 그냥 class는 자바스크립트의 `class` 라고 인식.
+- class를 className으로
+- HTML, CSS와 JS에서 같은 이름으로 다른 기능을 하는 것이 있다
+  - ex: class, for → className, htmlFor
+
+<br>
+
+### summary 글자수 동등하게 해주는 방법
+
+- string은 Array와 같다
+- string.length: 길이 반환
+- string.slice(start, end)
+
+```jsx
+<p className='movie__summary'>{summary.slice(0, 180)}...</p>
+```
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -8,14 +10,42 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json');
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className='container'>
+        {isLoading ? (
+          <div className='loader'>
+            <span className='loader__text'>Loading...</span>
+          </div>
+        ) : (
+          <div className='movies'>
+            {movies.map((movie) => (
+              <Movie
+                // key는 필수 props
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
